04a012a8699e754156fa6db5e628e18137dbd679
ruohan.liu <ruohan.lrh@gmail.com>
2022-06-12 10:34:45 -0700

    Update 0751 ipToCIDR.py

--- python/0751 ipToCIDR.py
+++ python/0751 ipToCIDR.py
@@ -1,20 +1,28 @@
 from typing import List
 import math
-import ipaddress
 class Solution:
     def ipToCIDR(self, ip: str, n: int) -> List[str]:
+        """
+            #math #bitwise #ipaddress #two'scomplement #important
+
+            value of least significan 1 in binary can be computed as x & (-x)
+            corner case: x = 0   0&0 = 0
+        """
+        import ipaddress
         ans = []
-        divisor = 2**10
-        ip_dec = sum(int(x)*256**(3-i) for i,x in enumerate(ip.split(".")))
-        while ip_dec % divisor > 0:
-            print(divisor,ip_dec % divisor,)
-            divisor //= 2
-
-        mask = 32-int(math.log(divisor,2))-1
-
-        if n >= divisor:
-            ans.append(str(ipaddress.IPv4Address(ip_dec & (1 << mask)))+f"/{mask}")
-        print(ip_dec,mask,ans)
-
+        ip_start = int(ipaddress.IPv4Address(ip))
+        while n > 0:
+            cap = ip_start & (-ip_start)
+            if cap == 0:
+                cap = 1024
+            while cap > n:
+                cap >>= 1
+            n -= cap
+            r = int(math.log(cap, 2))
+            l = 32-r
+            ans.append(str(ipaddress.IPv4Address(ip_start >> r << r))+f"/{l}")
+            ip_start += cap
+        return ans
+            
 s = Solution()
-print(s.ipToCIDR("255.0.0.8",12))
+print(s.ipToCIDR("0.0.0.0",10))

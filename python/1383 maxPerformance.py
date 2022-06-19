6fd115afa84678e6589911763f7d774d1d3f5247
ruohan.liu <ruohan.lrh@gmail.com>
2022-06-18 19:07:13 -0700

    Create 1383 maxPerformance.py

---
+++ python/1383 maxPerformance.py
@@ -0,0 +1,28 @@
+from typing import List
+class Solution:
+    def maxPerformance(self, n: int, speed: List[int], efficiency: List[int], k: int) -> int:
+        """
+            #sort #heap #hard #important
+
+            it is a two-dimension optimization problem. fix one dimension, enumerate on the other.
+            iterate from the greatest efficiency to allow one pass of candidates.
+
+            If curr_speed > popped speed, cur_speed should be in the heap.
+            If curr_speed < popped_speed, it means that it will be the new minimum and will be popped during next iteration anyway.
+        """
+        import heapq
+
+        mod = 10**9 + 7
+        candidates = sorted(zip(efficiency, speed), key=lambda x: x[0], reverse=True)
+
+        ans = 0
+        heap = []
+        speed_sum = 0
+        for eff,spe in candidates:
+            heapq.heappush(heap,spe)
+            speed_sum += spe
+            if len(heap) > k:
+                speed_sum -= heapq.heappop(heap)
+            
+            ans = max(ans,eff*speed_sum)
+        return ans % mod

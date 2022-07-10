3f5b548ac30e5ff7fe64e3a529f48ac8e2a73481
ruohan.liu <ruohan.lrh@gmail.com>
2022-07-10 11:21:10 -0700

    Create 0274 hIndex.py

---
+++ python/0274 hIndex.py
@@ -0,0 +1,34 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        """
+            #countsort #algorithm
+        """
+        citations.sort()
+        lo = 0
+        hi = citations[-1]
+        n = len(citations)
+        
+        while lo < hi :
+            mid = (lo+hi) // 2 + 1
+            if n - bisect.bisect_left(citations,mid) >= mid:
+                lo= mid
+            else:
+                hi = mid - 1
+                
+        return lo
+        
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations) 
+        # h index is at most n
+        cnt = [0] * (n+1)
+        for c in citations:
+            cnt[min(n,c)] += 1
+        
+        h = n
+        papers = 0 
+        while True:
+            papers += cnt[h]
+            if papers < h:
+                h -= 1
+            else:
+                return h

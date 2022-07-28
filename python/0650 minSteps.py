c33e624b51189cbe2c2ed6d6a50e2a56da7d0755
ruohan.liu <ruohan.lrh@gmail.com>
2022-07-27 18:59:49 -0700

    Create 0650 minSteps.py

---
+++ python/0650 minSteps.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minSteps(self, n: int) -> int:
+        """
+            #math #primefactor
+        """
+        ans = 0
+        d = 2
+        while n > 1:
+            while n % d == 0:
+                ans += d
+                n //= d
+            d += 1
+        return ans

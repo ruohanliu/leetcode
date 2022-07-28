2aaf60138ffbbe4eda3abd95d19b2c35b5a969c5
ruohan.liu <ruohan.lrh@gmail.com>
2022-07-27 18:59:41 -0700

    Update 0718 findLength.py

--- python/0718 findLength.py
+++ python/0718 findLength.py
@@ -26,3 +26,25 @@ class Solution:
                     curr[j] = 0
             curr, prev = prev, curr
         return ans
+
+    def findLength_tle(self, nums1: List[int], nums2: List[int]) -> int:
+        @lru_cache(maxsize=None)
+        def dp(i,j):
+            nonlocal ans
+            if i == 0 or j == 0:
+                return 0
+
+            dp(i-1,j)
+            dp(i,j-1)
+            if nums1[i-1] == nums2[j-1]:
+                res = dp(i-1,j-1) + 1
+                ans = max(ans,res)
+                return res
+            else:
+                return 0
+            
+        m = len(nums1)
+        n = len(nums2)
+        ans = 0
+        dp(m,n)
+        return ans

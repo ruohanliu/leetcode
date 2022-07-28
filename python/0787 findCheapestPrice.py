f3e9619217f454455290ea53bfb4d4c32c820ceb
ruohan.liu <ruohan.lrh@gmail.com>
2022-07-27 18:59:39 -0700

    Update 0787 findCheapestPrice.py

--- python/0787 findCheapestPrice.py
+++ python/0787 findCheapestPrice.py
@@ -25,3 +25,27 @@ class Solution:
                     dist[_stop][_v] = _cost
                     heapq.heappush(heap,(_cost,_stop,_v))
         return -1
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        """
+            #bellman-ford #important
+        """
+        if src == dst:
+            return 0
+        
+        INF = float("inf")
+        previous = [INF] * n
+        current = [INF] * n
+        previous[src] = 0
+        
+        for i in range(1, k + 2):
+            current[src] = 0
+            for flight in flights:
+                previous_flight, current_flight, cost = flight
+
+                if previous[previous_flight] < INF:
+                    current[current_flight] = min(current[current_flight],
+                                                  previous[previous_flight] + cost)
+                    
+            previous = current.copy()
+            
+        return -1 if current[dst] == INF else current[dst]

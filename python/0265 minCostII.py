0d25a44eca936836507769351282941f097ce575
ruohan.liu <ruohan.lrh@gmail.com>
2022-07-28 19:12:59 -0700

    Update 0265 minCostII.py

--- python/0265 minCostII.py
+++ python/0265 minCostII.py
@@ -14,3 +14,26 @@ class Solution:
                 curr[color] = ((cost1 if color != color1 else cost2) + costs[i][color],color)
                 
         return min(cost for cost,color in curr)
+
+    def minCostII(self, costs: List[List[int]]) -> int:
+        """
+            #dp #optimized
+        """
+        def heappush(heap,item):
+            if len(heap) < 2:
+                heapq.heappush(heap,item)
+            else:
+                heapq.heappushpop(heap,item)
+
+        n = len(costs)
+        k = len(costs[0])
+        prev = []
+        for idx,cost in enumerate(costs[0]):
+            heappush(prev,(-cost,idx))
+        for i in range(1,n):
+            curr = []
+            for color in range(k):
+                heappush(curr,((prev[1][0] if color != prev[1][1] else prev[0][0]) - costs[i][color],color))
+            prev,curr = curr,prev
+                
+        return min(-cost for cost,_ in prev)

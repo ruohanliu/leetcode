f0eae63c2f941b78d733dc1d3cff8dce058a6283
ruohan.liu <ruohan.lrh@gmail.com>
2022-08-10 23:23:56 -0700

    Update 0887 superEggDrop.py

--- python/0887 superEggDrop.py
+++ python/0887 superEggDrop.py
@@ -43,6 +43,7 @@ class Solution:
         dp = [[0]*(k+1) for _ in range(n+1)]
         for i in range(1,n+1):
             for j in range(1,k+1):
+                # egg breaks and doesnt break
                 dp[i][j] = dp[i-1][j-1] + dp[i-1][j] + 1
             if dp[i][k] >= n:
                 return i

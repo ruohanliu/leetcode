931b38d8f117cede900164eefb137ea6aa32d8f9
ruohan.liu <ruohan.lrh@gmail.com>
2022-08-12 12:45:03 -0700

    Update 0227 calculate.py

--- python/0227 calculate.py
+++ python/0227 calculate.py
@@ -1,51 +1,30 @@
 class Solution:
     """
-        #
+        #design
     """
     def calculate(self, s: str) -> int:
-        def operate(a,b,op):
-            if op == "+":
-                return a+b
-            elif op == "-":
-                return a-b
-            elif op == "*":
-                return a*b
-            else:
-                return a//b
-            
-        ans = 0
-        stack = deque()
-        i = 0
-        number = []
-        while i < len(s):
-            if not s[i].isdigit():
-                if s[i] in "+-/*":
-                    stack.append(s[i])
-                i += 1
-            else:
-                while i < len(s) and s[i].isdigit():
-                    number.append(s[i])
-                    i+=1
-                b = int("".join(number))
-                number.clear()
-                if stack and stack[-1] in "/*":
-                    op = stack.pop()
-                    a = stack.pop()
-                    b = operate(a,b,op)
-                stack.append(b)
-
-                while len(stack) >= 4:
-                    if str(stack[0]) in "+-":
-                        op = stack.popleft()
-                        if op == "-":
-                            stack[0] *= -1
+        if not s:
+            return 0
+        num = 0
+        sign = "+"
+        stack = []
+        for i,x in enumerate(s):
+            if x.isdigit():
+                num = num * 10 + int(x)
+            if (not x.isdigit() and not x.isspace()) or i == len(s) - 1:
+                if sign == "+":
+                    stack.append(num)
+                elif sign == "-":
+                    stack.append(-num)
+                elif sign == "*":
+                    stack.append(stack.pop()*num)
+                else:
+                    q,m = divmod(stack.pop(),num)
+                    if q<0 and m != 0:
+                        stack.append(q+1)
                     else:
-                        a = stack.popleft()
-                        op = stack.popleft()
-                        stack[0] = operate(a,stack[0],op)
-        if len(stack) == 1:
-            return stack[0]
-        elif len(stack) == 2:
-            return operate(0,stack[0],stack[1])
-        else:
-            return operate(stack[0],stack[2],stack[1])
+                        stack.append(q)
+                
+                sign = x
+                num = 0
+        return sum(stack)

6fb3d54c0dcc4d9deb1bc24fb310047bca0cc1a9
ruohan.liu <ruohan.lrh@gmail.com>
2022-08-14 14:11:07 -0700

    Update 0315 countSmaller.py

--- python/0315 countSmaller.py
+++ python/0315 countSmaller.py
@@ -1,36 +1,42 @@
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
         """
-            #sortedlist #mergesort #segmenttree #fenwicktree #furtherstudy
-
-
+            #sortedlist #segmenttree #fenwicktree
             https://leetcode.com/problems/count-of-smaller-numbers-after-self/solution/
+            
+            related 2179
         """
         from sortedcontainers import SortedList
-        output = [0] * len(nums)
-        nums_sorted = SortedList(nums)
-        for i in range(len(nums)):
-            output[i] = nums_sorted.bisect_left(nums[i])
-            nums_sorted.pop(output[i])
-        return output
+        sl = SortedList()
+        n = len(nums)
+        ans = [0] * n
+        for i in reversed(range(n)):
+            j = sl.bisect_left(nums[i])
+            ans[i] = j
+            sl.add(nums[i])
+        return ans
 
     def countSmaller(self, nums: List[int]) -> List[int]:
-        rank, N, res = {val: i + 1 for i, val in enumerate(sorted(nums))}, len(nums), []
-        BITree = [0] * (N + 1)
+        def add(i,delta):
+            i = i + 1
+            while i < n+1:
+                bit[i] += delta
+                i += i&-i
 
-        def update(i):
-            while i <= N:
-                BITree[i] += 1
-                i += (i & -i)
-
-        def getSum(i):
-            s = 0
+        def query(i):
+            res = 0
+            i = i + 1
             while i:
-                s += BITree[i]
-                i -= (i & -i)
-            return s
+                res += bit[i]
+                i -= i&-i
+            return res
 
-        for x in reversed(nums):
-            res += getSum(rank[x] - 1),
-            update(rank[x])
-        return res[::-1]
+        n = len(nums)
+        bit = [0] * (n+1)
+        ans = [0] * n
+        idxInSorted = {x: i for i, x in enumerate(sorted(nums))}
+        for i in reversed(range(n)):
+            rank = idxInSorted[nums[i]]
+            ans[i] = query(rank-1)
+            add(rank,1)
+        return ans

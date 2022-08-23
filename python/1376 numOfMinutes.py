4219c1e1c6ca2fc64e4ddad31c0cf5917ab6e34f
ruohan.liu <ruohan.lrh@gmail.com>
2022-08-23 00:36:02 -0700

    Update 1376 numOfMinutes.py

--- python/1376 numOfMinutes.py
+++ python/1376 numOfMinutes.py
@@ -1,22 +1,19 @@
 class Solution:
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
         """
-            #recursion #important
+            #dfs
 
             Time Needed to Inform All Employees
         """
         def dfs(v):
-            if not adjList[v]:
-                return 0
+            return informTime[v] + max([dfs(_v) for _v in sub[v]],default = 0)
 
-            return max(dfs(_v) for _v in adjList[v])+informTime[v]
-                
-        ans = 0
-        adjList = defaultdict(list)
-        for v in range(n):
-            adjList[manager[v]] += v,
+        sub = defaultdict(list)
+        for i in range(n):
+            sub[manager[i]] += i,
         
         return dfs(headID)
+
     
     def numOfMinutes_bad(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
         def dfs(v,t):

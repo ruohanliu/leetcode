f5b2cd803faa7ebb0160360304c1233ad876b496
ruohan.liu <ruohan.lrh@gmail.com>
2022-08-26 11:56:50 -0700

    Update 1004 longestOnes.py

--- python/1004 longestOnes.py
+++ python/1004 longestOnes.py
@@ -14,3 +14,15 @@ class Solution:
                 left+=1
         return right - left + 1
             
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        i = 0
+        ans = float("-inf")
+        for j,x in enumerate(nums):
+            if x == 0:
+                k-=1
+            while k < 0:
+                if nums[i] == 0:
+                    k += 1
+                i+=1
+            ans = max(ans,j-i+1)
+        return ans

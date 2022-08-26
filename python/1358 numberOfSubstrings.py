be3d17c9d5782101677b7407466e8e73b02839af
ruohan.liu <ruohan.lrh@gmail.com>
2022-08-26 11:56:44 -0700

    Create 1358 numberOfSubstrings.py

---
+++ python/1358 numberOfSubstrings.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        """
+            #slidingwindow
+            related 1248
+        """
+    def numberOfSubstrings(self, s: str) -> int:
+        ans = 0
+        i = 0
+        c = {x:0 for x in "abc"}
+        for j,x in enumerate(s):
+            c[x] += 1
+            while all(c[k] for k in c):
+                c[s[i]] -= 1
+                i += 1
+            ans += i
+        return ans

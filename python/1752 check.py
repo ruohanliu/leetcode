170ba213383fac30001bbb1fdb2ed3b6df89dac6
ruohan.liu <ruohan.lrh@gmail.com>
2022-08-31 12:14:02 -0700

    Create 1752 check.py

---
+++ python/1752 check.py
@@ -0,0 +1,6 @@
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        """
+            #clever
+        """
+        return sum(a > b for a, b in zip(nums, nums[1:] + nums[:1])) <= 1

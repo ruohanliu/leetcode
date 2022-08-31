31066915798aabdf61af304ff72c1e7ef49b0972
ruohan.liu <ruohan.lrh@gmail.com>
2022-08-31 12:14:48 -0700

    Update 0166 fractionToDecimal.py

--- python/0166 fractionToDecimal.py
+++ python/0166 fractionToDecimal.py
@@ -1,42 +1,21 @@
 class Solution:
     def fractionToDecimal(self, numerator: int, denominator: int) -> str:
         """
-            #math
+            #math #division
         """
-        ans = []
-        seen = {}
-        i = 0
-        decimal = False
-        negativesign = "-" if math.copysign(1,numerator) != math.copysign(1,denominator) else ""
+        sign = '-' if numerator*denominator < 0 else ''
         numerator = abs(numerator)
         denominator = abs(denominator)
-        
-        while True:
-            if numerator == 0:
-                break
-            while numerator < denominator:
-                numerator *= 10
-                ans.append("0")
-                i += 1
-            if decimal:
-                ans.pop()
-                i-=1
-                if numerator in seen:
-                    break
-                seen[numerator] = i
-            q,m = divmod(numerator,denominator)
-            ans.append(str(q))
-            i+=1
-            numerator = m
-            decimal = True
+        n, remainder = divmod(numerator, denominator)
 
-        if numerator == 0:
-            if not ans:
-                return "0"
-            elif len(ans) > 1:
-                return negativesign+ans[0]+"."+"".join(ans[1:])
-            else:
-                return negativesign+ans[0]
-        else:
-            return negativesign+ans[0]+"."+"".join(ans[1:seen[numerator]]+["("]+ans[seen[numerator]:]+[")"])
-            
+        ans = [sign+str(n), '.']
+        seen = {}
+        while remainder and remainder not in seen:
+            seen[remainder] = len(ans)
+            n, remainder = divmod(remainder*10, denominator)
+            ans.append(str(n))
+
+        if remainder:
+            ans.insert(seen[remainder], '(')
+            ans.append(')')
+        return ''.join(ans).rstrip('.')

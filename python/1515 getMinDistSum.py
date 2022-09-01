74749331ab98891ea9a34f1bbff7186f7ed5ffd3
ruohan.liu <ruohan.lrh@gmail.com>
2022-09-01 13:01:30 -0700

    Create 1515 getMinDistSum.py

---
+++ python/1515 getMinDistSum.py
@@ -0,0 +1,32 @@
+from turtle import position
+
+
+class Solution:
+    def getMinDistSum(self, positions: List[List[int]]) -> float:
+        """
+            #geometricmedian #proximation #centroid
+        """
+        fn = lambda x,y: sum(math.sqrt((x-i)**2+(y-j)**2) for i,j in positions)
+        n = len(positions)
+        x = 0
+        y = 0
+        for i,j in positions:
+            x += i
+            y += j
+        x /= n
+        y /= n
+        delta = 100
+        ans = fn(x,y)
+        while delta > 1e-6:
+            zoom = True
+            for dx,dy in ((1,0),(-1,0),(0,1),(0,-1)):
+                candidate = fn(x+dx*delta,y+dy*delta)
+                if candidate < ans:
+                    x += dx*delta
+                    y += dy*delta
+                    zoom = False
+                    ans = candidate
+                    break
+            if zoom:
+                delta /= 2
+        return ans

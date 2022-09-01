5ac112cac28e528c7d3be5bae93048576ec299a2
ruohan liu <ruohan.lrh@gmail.com>
2022-09-01 09:52:54 -0700

    Create 0465 minTransfers.py

---
+++ python/0465 minTransfers.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minTransfers(self, transactions: List[List[int]]) -> int:
+        """
+            #dp #relation
+        """
+        tuplify = lambda balance,fn: tuple(sorted((p,b) for p,b in balance.items() if fn(p)))
+        @cache
+        def dp(balance):
+            if not balance:
+                return 0
+            ans = float("inf")
+            balance = {p:b for p,b in balance}
+            for size in range(2,len(balance)+1):
+                for group in combinations(balance.keys(),size):
+                    if sum(balance[p] for p in group) == 0:
+                        ans = min(ans,size-1 + dp(tuplify(balance,lambda x:x not in group)))
+            return ans
+        
+        balance = defaultdict(int)
+        c = defaultdict(list)
+        for f,t,amt in transactions:
+            balance[f] -= amt
+            balance[t] += amt
+        temp = [(p,b) for p,b in balance.items()]
+        ans = 0
+        for p,b in temp:
+            if b == 0:
+                del balance[p]
+            elif c[-b]:
+                del balance[p]
+                del balance[c[-b].pop()]
+                ans += 1
+            else:
+                c[b].append(p)
+        return ans + dp(tuplify(balance,lambda x:True))

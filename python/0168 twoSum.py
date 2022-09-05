bc80e950eafe5232b986946f4ee08f62f1cb05e2
ruohan.liu <ruohan.lrh@gmail.com>
2022-09-05 10:15:16 -0700

    Update 0168 twoSum.py

--- python/0168 twoSum.py
+++ python/0168 twoSum.py
@@ -4,18 +4,19 @@ class Solution:
         """
         numbers is sorted
         """
-
         lo = 0
         hi = len(numbers) - 1
 
         while lo<hi:
             if numbers[lo] + numbers[hi] < target:
+                while lo < hi and numbers[lo+1] == numbers[lo]:
+                    lo += 1
                 lo += 1
             elif numbers[lo] + numbers[hi] > target:
+                while lo < hi and numbers[hi-1] == numbers[hi]:
+                    hi -= 1
                 hi -= 1
             else:
                 return [lo+1,hi+1]
 
         return []
-
-        

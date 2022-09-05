2de391754aed503d2bd4f9bdf0563feb124a6d44
ruohan.liu <ruohan.lrh@gmail.com>
2022-09-05 10:14:26 -0700

    Create 1218 longestSubsequence.py

---
+++ python/1218 longestSubsequence.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestSubsequence_tle(self, arr: List[int], difference: int) -> int:
+        """
+            #dp
+        """
+        dp = defaultdict(int)
+        for num in arr:
+            dp[num] = max(dp[num], 1+dp[num-difference])
+        return max(dp.values())
+
+    def longestSubsequence_tle(self, arr: List[int], difference: int) -> int:
+        """
+            #lis
+        """
+        n = len(arr)
+        dp = [1]*n
+        for i in range(1,n):
+            for j in range(i):
+                if arr[i] - arr[j] == difference:
+                    dp[i] = max(dp[i],dp[j]+1)
+        return max(dp)

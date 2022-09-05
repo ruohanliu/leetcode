392cdac9cf923d26c9b648882f848dda0d94a26d
ruohan.liu <ruohan.lrh@gmail.com>
2022-09-05 10:15:09 -0700

    Update 0236 lowestCommonAncestor.py

--- python/0236 lowestCommonAncestor.py
+++ python/0236 lowestCommonAncestor.py
@@ -8,8 +8,8 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         """
-            #tree #lca
-            related 1740
+            #tree #lca #binarylifting
+            related 1740 2277
         """
         def dfs(root):
             if root:
@@ -24,3 +24,47 @@ class Solution:
                 return left or right
 
         return dfs(root)
+
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def dfs(node,parents,depth):
+            nonlocal dp,depths,tree
+            if node:
+                tree[node.val] = node
+                depths[node.val] = depth
+                for bit in range(m):
+                    if 1<<bit <= len(parents):
+                        dp[node.val][bit] = parents[-(1<<bit)]
+                parents.append(node.val)
+                if node.left:
+                    dfs(node.left,parents,depth+1)
+                if node.right:
+                    dfs(node.right,parents,depth+1)
+                parents.pop()
+        def kAncestor(val,k):
+            while k:
+                bit = k.bit_length() - 1
+                val = dp[val][bit]
+                k -= 1<<bit
+            return val
+
+        def lca(a,b):
+            if depths[a] > depths[b]:
+                return lca(b,a)
+            b = kAncestor(b,depths[b] - depths[a])
+            lo = 0
+            hi = depths[a]
+            while lo < hi:
+                mid = (lo+hi) // 2
+                if kAncestor(a,mid) == kAncestor(b,mid):
+                    hi = mid
+                else:
+                    lo = mid + 1
+            return kAncestor(a,lo)
+
+        n = 10**5
+        m = n.bit_length()
+        tree = {}
+        dp = defaultdict(lambda: [0]*m)
+        depths = defaultdict(int)            
+        dfs(root,[],0)
+        return tree[lca(p.val,q.val)]

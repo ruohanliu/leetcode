a6bbd3b5491f4f53cf2f9f563e5e4bc9808c0c52
ruohan.liu <ruohan.lrh@gmail.com>
2022-09-05 10:14:50 -0700

    Update 0560 subarraySum.py

--- python/0560 subarraySum.py
+++ python/0560 subarraySum.py
@@ -2,15 +2,18 @@ from typing import List
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         """
+            #prefixsum
+
             naive method: two loops, cumulative sum, O(n^2)
 
             best method: hashmap, store cumulative sum as key,
                 one pass, O(n) and O(n)
                 current sum - previous sum  = 1 current index solution
                 s - (s-k) = k
-                #dp #difficult #clever 
 
             cannot use sliding window because of negative values
+
+            related 325 1477
         """
         ans = 0
         s = 0
@@ -25,6 +28,4 @@ class Solution:
                 d[s] += 1
         return ans
 s = Solution()
-print(s.subarraySum([1,2,3,0,-1,-2,1,3],2))
-
-        
+print(s.subarraySum([1,2,3,0,-1,-2,1,3],2))      

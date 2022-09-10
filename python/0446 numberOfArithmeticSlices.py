8fe533f9c66503ec75de5e59ded12323cbb0140a
ruohan.liu <ruohan.lrh@gmail.com>
2022-09-09 22:26:59 -0700

    Update 0446 numberOfArithmeticSlices.py

--- python/0446 numberOfArithmeticSlices.py
+++ python/0446 numberOfArithmeticSlices.py
@@ -1,7 +1,7 @@
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
         """
-            #dp #relation
+            #dp #relation #arithmetic
         """
         n = len(nums)
         if n == 2:
@@ -9,9 +9,10 @@ class Solution:
         ans = 0
         # dp[i][k] number of weak arithmetic subsequence with common diff k and ending at i
         dp = [defaultdict(int) for _ in range(n)]
-        for i in range(n):
-            for j in range(i):
-                diff = nums[i]-nums[j]
-                dp[i][diff] += dp[j][diff] + 1
-                ans += dp[j][diff]
+        for j in range(n):
+            for i in range(j):
+                diff = nums[j]-nums[i]
+                dp[j][diff] += dp[i][diff] + 1
+                # for a given i and diff, number of weak arithmetic subsequence is 1 larger
+                ans += dp[i][diff]
         return ans

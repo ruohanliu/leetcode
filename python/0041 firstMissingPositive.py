e13f0e0c391e4e4be64019b3aebeb13bfb882676
ruohan.liu <ruohan.lrh@gmail.com>
2022-09-09 22:28:00 -0700

    Update 0041 firstMissingPositive.py

--- python/0041 firstMissingPositive.py
+++ python/0041 firstMissingPositive.py
@@ -6,6 +6,8 @@ class Solution:
         n = len(nums)
         
         one = False
+        # the answer must be in [1,n+1]
+        # 1st pass setting out of range to 1
         for i in range(n):
             if nums[i] == 1:
                 one = True
@@ -14,14 +16,13 @@ class Solution:
         if not one:
             return 1
         
+        # 2nd pass: setting related index to negative
         for i in range(n):
             x = abs(nums[i])
-            if n>= x > 0:
-                nums[x-1] = - abs(nums[x-1])
+            nums[x-1] = - abs(nums[x-1])
         
+        # 3rd pass: any still positive number is the answer
         for i in range(n):
             if nums[i] > 0:
                 return i+1
         return n+1
-        
-                

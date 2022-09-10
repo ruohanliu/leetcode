75fdb97578d417f3a251c34d3a15c2f95d85bc51
ruohan.liu <ruohan.lrh@gmail.com>
2022-09-09 22:26:43 -0700

    Update 0751 ipToCIDR.py

--- python/0751 ipToCIDR.py
+++ python/0751 ipToCIDR.py
@@ -3,7 +3,7 @@ import math
 class Solution:
     def ipToCIDR(self, ip: str, n: int) -> List[str]:
         """
-            #math #bitwise #ipaddress #two'scomplement #important
+            #math #bitwise #ipaddress #two'scomplement
 
             value of least significan 1 in binary can be computed as x & (-x)
             corner case: x = 0   0&0 = 0
@@ -23,6 +23,33 @@ class Solution:
             ans.append(str(ipaddress.IPv4Address(ip_start >> r << r))+f"/{l}")
             ip_start += cap
         return ans
-            
-s = Solution()
-print(s.ipToCIDR("0.0.0.0",10))
+
+    def ipToCIDR(self, ip: str, n: int) -> List[str]:
+        def ipToInt(ip):
+            ans = 0
+            for i,block in enumerate(ip.split(".")):
+                ans += int(block) << (8 * (3-i))
+            return ans
+        def intToIP(i):
+            ans = []
+            for _ in range(4):
+                i,m = divmod(i,256)
+                ans.append(str(m))
+            return ".".join(ans[::-1])
+
+        ans = []
+        ip_start = ipToInt(ip)
+        while n > 0:
+            rbit = ip_start & (-ip_start)
+            # edge case when input ip is 0, max n is 1000
+            if rbit == 0:
+                rbit = 1024
+            while rbit > n:
+                rbit >>= 1
+            n -= rbit
+            # r is the length of masked bits
+            r = rbit.bit_length()-1
+            l = 32-r
+            ans.append(intToIP(ip_start >> r << r)+f"/{l}")
+            ip_start += rbit
+        return ans

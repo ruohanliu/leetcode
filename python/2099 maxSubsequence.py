40a365506e6c7d8f7f3df33cf3bd33f932ec1884
ruohan liu <ruohan.lrh@gmail.com>
2022-09-09 17:47:38 -0700

    Update 2099 maxSubsequence.py

--- python/2099 maxSubsequence.py
+++ python/2099 maxSubsequence.py
@@ -19,3 +19,45 @@ class Solution:
                 ans.append(n)
                 c[n] -=1
         return ans
+
+    def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
+        """
+            #quickselect
+        """
+        def quickselect(lo,hi,k):
+            def partition(lo,hi):
+                i=lo
+                pivot = nums[random.randrange(lo,hi+1)]
+                while i <= hi:
+                    if nums[i] < pivot:
+                        nums[i],nums[lo] = nums[lo],nums[i]
+                        i+=1
+                        lo+=1
+                    elif nums[i]> pivot:
+                        nums[i],nums[hi] = nums[hi],nums[i]
+                        hi-=1
+                    else:
+                        i+=1
+                return lo,hi
+                
+            if lo>=hi:
+                return lo,lo
+            lt,gt = partition(lo,hi)
+            # comment out for quicksort
+            if lt<=k<=gt:
+                return lt,gt
+            if k>gt:
+                return quickselect(gt+1,hi,k)
+            else:
+                return quickselect(lo,lt-1,k)
+
+        orig = nums[:]
+        quickselect(0,len(nums)-1,len(nums)-k)
+        
+        c = Counter(nums[-k:])
+        ans = []
+        for x in orig:
+            if c[x]:
+                ans.append(x)
+                c[x] -= 1
+        return ans

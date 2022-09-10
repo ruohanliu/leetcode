dee3a9c4565a317ce9d692a44691433b4d74def0
ruohan.liu <ruohan.lrh@gmail.com>
2022-09-09 22:26:31 -0700

    Update 0937 reorderLogFiles.py

--- python/0937 reorderLogFiles.py
+++ python/0937 reorderLogFiles.py
@@ -19,4 +19,4 @@ class Solution:
             _id,rest = x.split(maxsplit = 1)
             return (0,rest,_id) if rest[0].isalpha() else (1,)
 
-        return sorted(logs)
+        return sorted(logs,key = get_key)

a4124ff7fff4519a3c83e92d8cdbf00f21b38d28
ruohan.liu <ruohan.lrh@gmail.com>
2022-09-11 00:56:15 -0700

    Update 0078 subsets.py

--- python/0078 subsets.py
+++ python/0078 subsets.py
@@ -3,6 +3,14 @@ class Solution:
         return [[x for j,x in enumerate(nums) if 1 << j & i] for i in range(2**(len(nums)))]
 
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = [[]]
+        for x in nums:
+            new = [subset+[x] for subset in res]
+            res.extend(new)
+        
+        return res
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
         def add(x):
             nonlocal res
             res += [subset + [x] for subset in res]

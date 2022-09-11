ccb7e375224c60a8c9b2d59f19242caa81c69fd4
ruohan.liu <ruohan.lrh@gmail.com>
2022-09-11 00:55:54 -0700

    Update 0940 distinctSubseqII.py

--- python/0940 distinctSubseqII.py
+++ python/0940 distinctSubseqII.py
@@ -2,6 +2,8 @@ class Solution:
     def distinctSubseqII(self, s: str) -> int:
         """
             #dp #relation #important #subsequence
+
+            related 828
         """
         dp = [1]
         last = {}
@@ -15,3 +17,15 @@ class Solution:
         return (dp[-1] - 1) % (10 ** 9 + 7)
 
         
+    def distinctSubseqII(self, s):
+        end = [0] * 26
+        for c in s:
+            end[ord(c) - ord('a')] = sum(end) + 1
+        return sum(end) % (10**9 + 7)
+
+    def distinctSubseqII(self, s):
+        end = [0] * 26
+        ans = 0
+        for c in s:
+            end[ord(c) - ord('a')],ans = ans+1,2*ans+1-end[ord(c) - ord('a')]
+        return ans % (10**9 + 7)

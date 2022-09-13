b52b617c902bc5adff12558725813b7e56fc2c82
ruohan.liu <ruohan.lrh@gmail.com>
2022-09-12 20:53:28 -0700

    Create 0881 numRescueBoats.py

---
+++ python/0881 numRescueBoats.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        """
+            #greedy
+
+            related: 1986
+        """
+        people.sort()
+        i, j = 0, len(people) - 1
+        ans = 0
+        while i <= j:
+            ans += 1
+            if people[i] + people[j] <= limit:
+                i += 1
+            j -= 1
+        return ans
